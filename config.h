386c5d2ce13221a426b7171a875e304b91bbf84b
CatDevz <codyquinn1122@gmail.com>
2021-06-10 23:26:09 -0700

    :wrench: Changed keybindings

--- config.h
+++ config.h
@@ -2,13 +2,13 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 20;       /* snap pixel */
+static const unsigned int snap      = 40;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 1;
-static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 
-static const char *fonts[]          = { "Fantasque Sans Mono Nerd Font:size=14:antialias=true:autohint=true", };
+static const char *fonts[]          = { "Fantasque Sans Mono Nerd Font:size=15:antialias=true:autohint=true", "ttf-twemoji:size=15" };
 
 // Colors
 static const char col_gb_black[]	= "#131515";
@@ -47,16 +47,17 @@ static const char col_gb_orange2[]  = "#fe8019";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm]     = { col_gb_gray2,   col_gb_bgm,     col_gb_bg },
-	[SchemeSel]      = { col_gb_orange2, col_gb_bgm,     col_gb_black },
-	[SchemeSel2]     = { col_gb_fg,      col_gb_bg,     col_gb_bg },
-	[SchemeUrgent]   = { col_gb_fg,      col_gb_bg,     col_gb_bg },
-	[SchemeTitle]    = { col_gb_fg,      col_gb_bgs,    col_gb_bg },
-	[SchemeLt]       = { col_gb_orange2, col_gb_bgm,    col_gb_bg },
-	[SchemeNotify]   = { col_gb_red1,    col_gb_bg,     col_gb_bg },
-	[SchemeStatus]   = { col_gb_aqua2,   col_gb_bg,     col_gb_bg },
-	[SchemeIndOff]   = { col_gb_green2,  col_gb_bg,     col_gb_bg },
-	[SchemeIndOn]    = { col_gb_blue2,   col_gb_bg,     col_gb_bg },
+	[SchemeNorm]    = { col_gb_gray2,	col_gb_bgm,	col_gb_bgm },
+	[SchemeSel]     = { col_gb_orange2,	col_gb_bgm,	col_gb_bg },
+	[SchemeSelTag]	= { col_gb_orange2,	col_gb_bgm,	col_gb_bgm },
+	//[SchemeSel2]     = { col_gb_fg,      col_gb_bg,     col_gb_bg },
+	//[SchemeUrgent]   = { col_gb_fg,      col_gb_bg,     col_gb_bg },
+	//[SchemeTitle]    = { col_gb_fg,      col_gb_bgs,    col_gb_bg },
+	//[SchemeLt]       = { col_gb_orange2, col_gb_bgm,    col_gb_bg },
+	//[SchemeNotify]   = { col_gb_red1,    col_gb_bg,     col_gb_bg },
+	//[SchemeStatus]   = { col_gb_aqua2,   col_gb_bg,     col_gb_bg },
+	//[SchemeIndOff]   = { col_gb_green2,  col_gb_bg,     col_gb_bg },
+	//[SchemeIndOn]    = { col_gb_blue2,   col_gb_bg,     col_gb_bg },
 };
 
 /* tagging */
@@ -83,6 +84,8 @@ static const Layout layouts[] = {
 	{ "﩯 ",      tile },    /* first entry is default */
 	{ "",      NULL },    /* no layout function means floating behavior */
 	{ "",      monocle },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
@@ -120,14 +123,33 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 
 	// Multimedia
-	{ MODKEY,                       XK_F1,     spawn,          SHCMD("pulseaudio-ctl down 5 ; kill -44 $(pidof dwmblocks)") },
-	{ MODKEY,                       XK_F2,     spawn,          SHCMD("pulseaudio-ctl up 5 ; kill -44 $(pidof dwmblocks)") },
-	{ MODKEY,                       XK_F3,     spawn,          SHCMD("pulseaudio-ctl mute ; kill -44 $(pidof dwmblocks)") },
+	{ 0,                       0x1008ffb2,     spawn,          SHCMD("pulsemixer --id source-1 --toggle-mute ; kill -44 $(pidof dwmblocks)") },
+	{ 0,                       0x1008ff13,     spawn,          SHCMD("pulsemixer --change-volume +5 ; kill -44 $(pidof dwmblocks)") },
+	{ 0,                       0x1008ff11,     spawn,          SHCMD("pulsemixer --change-volume -5 ; kill -44 $(pidof dwmblocks)") },
+	{ 0,                       0x1008ff12,     spawn,          SHCMD("pulsemixer --set-volume 0 ; kill -44 $(pidof dwmblocks)") },
+	
+	{ 0,                       0x1008ff03,     spawn,          SHCMD("brightnessctl -d 'intel_backlight' set 5%-") },
+	{ 0,                       0x1008ff02,     spawn,          SHCMD("brightnessctl -d 'intel_backlight' set 5%+") },
+
+	{ MODKEY,			0xff52,	   spawn,	   SHCMD("mpv ~/Music/ --shuffle --no-video") },
+	{ MODKEY,			0xff54,	   spawn,	   SHCMD("playerctl play-pause") },
+	{ MODKEY,			0xff51,	   spawn,	   SHCMD("playerctl previous") },
+	{ MODKEY,			0xff53,	   spawn,	   SHCMD("playerctl next") },
+
+	{ MODKEY,			XK_e,	   spawn,	   SHCMD("splatmoji copy") },
 
   // Prompts
   { MODKEY|ShiftMask,             XK_x,      spawn,          SHCMD("~/.scripts/prompt \"Shutdown? \" \"shutdown now\"") },
-  { MODKEY|ControlMask,		  XK_x,	     spawn,	     SHCMD("~/.scripts/prompt \"Exit DWM? \" \"killall xinit\"") },
-  { MODKEY,		XK_x,	spawn,	SHCMD("~/.scripts/prompt \"Restart DWM? \" \"killall xinit && startx\"") },
+  { MODKEY|ControlMask,		  XK_x,	     spawn,	     SHCMD("~/.scripts/prompt \"Exit DWM? \" \"killall dwm\"") },
+
+	{ MODKEY,                       XK_Down,   moveresize,     {.v = "0x 25y 0w 0h" } },
+	{ MODKEY,                       XK_Up,     moveresize,     {.v = "0x -25y 0w 0h" } },
+	{ MODKEY,                       XK_Right,  moveresize,     {.v = "25x 0y 0w 0h" } },
+	{ MODKEY,                       XK_Left,   moveresize,     {.v = "-25x 0y 0w 0h" } },
+	{ MODKEY|ShiftMask,             XK_Down,   moveresize,     {.v = "0x 0y 0w 25h" } },
+	{ MODKEY|ShiftMask,             XK_Up,     moveresize,     {.v = "0x 0y 0w -25h" } },
+	{ MODKEY|ShiftMask,             XK_Right,  moveresize,     {.v = "0x 0y 25w 0h" } },
+	{ MODKEY|ShiftMask,             XK_Left,   moveresize,     {.v = "0x 0y -25w 0h" } },
 
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -142,6 +164,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,14 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	//{"", "~/.config/dwm/statusbar/_traffic.sh",		1,			0},
-  //{"", "~/.config/dwm/statusbar/_weather.sh",   120,    0},
-	{"", "~/.config/dwm/statusbar/_wifi.sh",			1,			0},
-	{"", "~/.config/dwm/statusbar/_audio.sh",			0,			10},
-	{"", "~/.config/dwm/statusbar/_cpu.sh",	  		1,			0},
-	{"", "~/.config/dwm/statusbar/_battery.sh",		1,			0},
-	{"", "~/.config/dwm/statusbar/_date.sh",			3600,		1},
-	{"", "~/.config/dwm/statusbar/_displaytime.sh",	1,			0},
+	//{"", "~/.config/dwm/statusbar/_traffic.sh",	1,		0},
+	//{"", "~/.config/dwm/statusbar/_weather.sh",	120,		0},
+	//{"", "~/.config/dwm/statusbar/_wifi.sh",	1,		0},
+	//{"", "~/.config/dwm/statusbar/_brightness.sh",	1,		0},
+	//{"", "~/.config/dwm/statusbar/_playerctl.sh",	1,		0},
+	//{"", "~/.config/dwm/statusbar/_info.sh",	10,		0},
+	{"", "~/.config/dwm/statusbar/_audio.sh",	30,		10},
+	{"", "~/.config/dwm/statusbar/_cpu.sh",	  	1,		0},
+	{"", "~/.config/dwm/statusbar/_battery.sh",	1,		0},
+	{"", "~/.config/dwm/statusbar/_date.sh",	3600,		1},
+	{"", "~/.config/dwm/statusbar/_displaytime.sh",	1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
